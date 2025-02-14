5087187ba37dcbfdff59d0efa6951caf355f5139
Arnav K <arnavkaushal09@gmail.com>
2025-02-14 07:33:04 +0000

    chore

--- index.ts
+++ index.ts
@@ -28,7 +28,6 @@ export default async function main() {
   const discordPoster = new DiscordPoster();
 
   const postManager = new PostManager([
-    twitterPoster,
     redditPoster,
     discordPoster,
   ]);
